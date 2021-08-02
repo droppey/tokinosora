e42f69c3ca68fad87fac8e7e35121270c0c05fae
drops <coco554211@gmail.com>
2021-08-02 18:26:20 +0800

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -6,7 +6,7 @@ class help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        await ctx.send("```Copypasta類：cbt , jojo , meme , wtm , 約嗎\n\n實用類：purge , say , sayd , spam , help\n\n選擇權評價(Black Scholes)：call , put (Spot , Strike , Time_to_Maturity , Interest_Rate , Sigma)\n\n數學：dty (Day to Year)\n\n你也許用不了類：load , reload , unload```")
+        await ctx.send("```Copypasta類：cbt , jojo , meme , 約嗎\n\n實用類：purge , say , sayd , spam , help\n\n選擇權評價(Black Scholes)：call , put (Spot , Strike , Time_to_Maturity , Interest_Rate , Sigma)\n\n數學：dty (Day to Year)\n\n你也許用不了類：load , reload , unload```")
 
 def setup(bot):
     bot.add_cog(help(bot))
