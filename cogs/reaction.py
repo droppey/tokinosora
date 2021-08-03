6a6190307e8dcc5841a04133ece8b76f97215b24
drops <coco554211@gmail.com>
2021-08-03 10:19:06 +0800

    ????

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -21,6 +21,14 @@ class reaction(commands.Cog):
         if msg.content == '您真愛說笑，關西電神':
                 await msg.channel.send('宜中電神瘋起來連自己都嘲諷 哀')
                 await msg.add_reaction('<:ran:869589093372289084>')
+        
+        if msg.content == '切嚕':
+            prob = random.randrange(11)
+            if prob == 10:
+                await msg.channel.send("再吵琪愛兒就把你們的切嚕洞塞進切嚕讓你們切嚕齒打顫得切嚕切嚕\n切嚕切嚕，切嚕啪，切嚕嚕嚕嚕♪\n切啦嚕咧，切啦切啦，切嚕切啵啪嗶~♪\n切嚕~☆切啦哩咧，切嚕啵嗶，切啦嚕啵啵\n切嚕切嚕，切嚕啪，切嚕嚕嚕嚕♪\n切嚕☆切嚕☆切嚕☆\n切嚕切嚕，切嚕啪，切嚕嚕嚕嚕♪\n切嚕~☆切啦哩咧，切嚕啵嗶，切啦嚕啵啵")
+            else: 
+                await msg.channel.send('切啦嚕咧，切啦切啦，切嚕切啵啪嗶~♪')
+
 
         if msg.content == '電' or msg.content == '電神' or msg.content == '⚡' or msg.content == 'electric' or msg.content == 'zap' :
             prob = random.randrange(11)
